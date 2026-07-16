d18dab0a7a95062b2754b94f569a1abaefa2640b
Lorris Saint-Genez <lorrissaintgenez@gmail.com>
2026-07-16 13:21:40 -0700

    refactor(setup): remove local setup scripts and ingestion lib

--- scripts/lib/log.mjs
+++
@@ -1,8 +0,0 @@
-export function log(step, msg) {
-  console.log(`\x1b[36m[${step}]\x1b[0m ${msg}`);
-}
-
-export function fail(msg) {
-  console.error(`\x1b[31m✗\x1b[0m ${msg}`);
-  process.exit(1);
-}
